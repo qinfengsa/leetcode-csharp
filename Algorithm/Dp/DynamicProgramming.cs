958f06d40ec47cf2feace53c8412ede801c5ddeb
wangheng <15800012123@163.com>
2023-05-16 18:10:59 +0800

    20230516 6. N 字形变换 1335. 工作计划的最低难度

---
+++ Algorithm/Dp/DynamicProgramming.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_csharp.Algorithm.Dp
+{
+    class DynamicProgramming
+    {
+        public int MinDifficulty(int[] jobDifficulty, int d)
+        {
+
+        }
+    }
+}
--- DataStructure/String/StringMain.cs
+++ DataStructure/String/StringMain.cs
@@ -102,5 +102,69 @@ namespace leetcode_csharp.DataStructure.String
 
             return result;
         }
+
+        // 6. N 字形变换
+        // 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+
+        // 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+        // P A   H N
+        // A P L S I I G
+        // Y   I R
+        // 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+
+        // 请你实现这个将字符串进行指定行数变换的函数： 
+        // string convert(string s, int numRows);
+
+
+        // 示例 1： 
+        // 输入：s = "PAYPALISHIRING", numRows = 3
+        // 输出："PAHNAPLSIIGYIR"
+
+        // 示例 2：
+        // 输入：s = "PAYPALISHIRING", numRows = 4
+        // 输出："PINALSIGYAHRPI"
+        // 解释：
+        // P I    N
+        // A   L S  I G
+        // Y A   H R
+        // P I
+
+        // 示例 3： 
+        // 输入：s = "A", numRows = 1
+        // 输出："A"
+
+        // 提示： 
+        // 1 <= s.length <= 1000
+        // s 由英文字母（小写和大写）、',' 和 '.' 组成
+        // 1 <= numRows <= 1000
+
+        public string Convert(string s, int numRows)
+        {
+            var n = s.Length;
+            if (n < 2)
+            {
+                return s;
+            }
+            var chars = new char[n];
+            var index = 0;
+            // nLen N 字形的长度 numRows == 
+            var nLen = numRows * 2 - 2;
+            for (int row = 0; row < numRows; row++)
+            {
+                var firstIdx = row;
+                var secondIdx = nLen - row;
+                while (firstIdx < n)
+                {
+                    chars[index++] = s[firstIdx];
+                    firstIdx += nLen;
+                    if (row > 0 && row < numRows - 1 && secondIdx < n)
+                    {
+                        chars[index++] = s[secondIdx];
+                        secondIdx += nLen;
+                    }
+                }
+            }
+            return new string(chars);
+        }
     }
 }
